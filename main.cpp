cc9bc62d19e1c1263f96b692142dfb862bedc7a2
Максим Костюков <enginer385@gmail.com>
2023-12-21 21:20:45 +1000

    Test windows build

--- main.cpp
+++ main.cpp
@@ -1,64 +1,32 @@
+include <iostream>
+#include "SharedMemory.hpp"
 #if defined(WIN32)
 #include "windows.h"
-#define THREAD_HANDLE HANDLE
 #else
-#include <pthread.h>
-#define DWORD void*
+#define DWORD unsigned long int
 #define WINAPI
 #define LPVOID void*
-#define THREAD_HANDLE pthread_t
 #endif
-#include <iostream>
-#include "SharedMemory.hpp"
-
 
 using namespace std;
 
-DWORD WINAPI ThreadFunc(SharedMemory<int>* shmem) {
-    shmem->Lock();
-    cout << shmem->content->data << endl;
-    shmem->content->data = 99;
-    shmem->Unlock();
-#if defined(WIN32)
+DWORD WINAPI ThreadFunc(LPVOID lpParam) {
     return 0;
-#else
-    return nullptr;
-#endif
 }
-
-#if !defined(WIN32)
-void Sleep(int milliseconds) {
-    usleep(milliseconds*1000);
-}
-#endif
 
 int main() {
-    auto shmem = new SharedMemory<int>("test_name");
-    shmem->Lock();
-    shmem->content->data = 55;
-    DWORD thread_id;
-    THREAD_HANDLE h;
-    #if defined(WIN32)
-        h = CreateThread(
+    auto shmem = SharedMemory<int>("test_name");
+    DWORD thread_id = 0;
+#if defined(WIN32)
+    CreateThread(
                 nullptr,
                 0,
-                reinterpret_cast<LPTHREAD_START_ROUTINE>(ThreadFunc),
-                shmem,
+                (LPTHREAD_START_ROUTINE)ThreadFunc,
+                nullptr,
                 0,
                 &thread_id
                 );
-    #else
-        pthread_attr_t attrs;
-        pthread_attr_init(&attrs);
-        pthread_create(&h, &attrs, reinterpret_cast<void *(*)(void *)>(ThreadFunc), shmem);
-    #endif
+#endif
 
-    cout << "Hello" << endl;
-    shmem->Unlock();
-    Sleep(1000);
-    shmem->Lock();
-    cout << shmem->content->data << endl;
-    shmem->Unlock();
-    
     return 0;
 }
